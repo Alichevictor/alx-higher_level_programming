b5a197d44f65093c201262a741681e6928a31feb
Alichevictor <alichevictor@gmail.com>
2022-09-07 11:14:27 +0100

    yes

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-const { argv } = require('process');
-const occurence = Number(argv[2]);
-const display = () => {
-  for (let i = 0; i < occurence; i++) {
-    console.log('C is fun');
+
+const lang = 'C is fun';
+
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log(lang);
   }
-};
-isNaN(occurence)
-  ? (console.log('Missing number of occurrences'))
-  : (display());
-  
+}
